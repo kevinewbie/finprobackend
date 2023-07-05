28e68d5c5c3616406332bc27161b45f4b58e9bca
Kevin Admansyah <56984565+kevinewbie@users.noreply.github.com>
2023-07-05 12:00:43 +0700

    Update Connect.go

--- model/Connect.go
+++ model/Connect.go
@@ -11,12 +11,12 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=containers-us-west-205.railway.app user=postgres password=A8MVAbgu58G5npdJzWQC dbname=railway port=6725 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+	dsn := "aprh8956_dbceria:dbceriangentot@tcp(103.247.8.36:3306)/aprh8956_dbceria?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
 	if err != nil {
-		fmt.Printf("Error")
+		fmt.Printf("Error: %v", err)
 		return
 	}
 	DB = db
